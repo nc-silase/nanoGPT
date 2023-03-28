a0d09468cd15ee4ecd9f3fce0a8ed30939cbf987
gitGut01 <silaseei@hotmail.com>
2023-03-28 08:56:55 +0200

    add log

--- data/finetune/prepare.py
+++ data/finetune/prepare.py
@@ -11,13 +11,6 @@ base_url = 'https://huggingface.co/silaseic/nanogpt_finetuned_models/resolve/mai
 def full_path(relative_pth):
     return os.path.join(os_path, relative_pth)
 
-#
-# input_file_path = os.path.join(os.path.dirname(__file__), 'input.txt')
-# if not os.path.exists(input_file_path):
-#     data_url = 'https://raw.githubusercontent.com/karpathy/char-rnn/master/data/tinyshakespeare/input.txt'
-#     with open(input_file_path, 'w') as f:
-#         f.write(requests.get(data_url).text)
-
 # choose dataset
 input_file_path = full_path('input.txt')
 if not os.path.exists(input_file_path):
