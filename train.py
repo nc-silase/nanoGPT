3472fec5e47be289576f418f023636369a19ca11
gitGut01 <silaseei@hotmail.com>
2023-04-03 16:00:39 +0200

    fix missing val from log

--- train.py
+++ train.py
@@ -254,7 +254,9 @@ with open('log.txt', 'w') as f:
         # evaluate the loss on train/val sets and write checkpoints
         if iter_num % eval_interval == 0 and master_process:
             losses = estimate_loss()
-            print(f"step {iter_num}: train loss {losses['train']:.4f}, val loss {losses['val']:.4f}")
+            log_text = f"step {iter_num}: train loss {losses['train']:.4f}, val loss {losses['val']:.4f}"
+            print(log_text)
+            f.write(f"{log_text}\n")
             if wandb_log:
                 wandb.log({
                     "iter": iter_num,
